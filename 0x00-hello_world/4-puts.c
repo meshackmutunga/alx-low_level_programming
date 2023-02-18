6622943923c63359cf3dee4a038b1b7b2a7b5662
Bob <bobphilip93@gmail.com>
2023-02-18 07:00:19 -0800

    Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
-/*
- * 4.Puts.c
- * Auth: Boby
- */
 #include <stdio.h>
 
-/*
- * main - Prints "Programming is like building a multilingual puzzle
- * then a new line
- *        
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
