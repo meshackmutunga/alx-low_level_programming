cba3acae2137e8a52b12707803d2491ce283ceed
Bob <bobphilip93@gmail.com>
2023-02-20 08:35:19 -0800

    nice work

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,10 @@
 /*
  * File: 0-positive_or_negative.c
- * Auth: Brennan D Baraban
  */
 
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 
 /**
  * main - Prints a random number and states whether
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,11 @@
 /*
  * File: 8-print_base16.c
- * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
 
 /**
  * main - Prints all the numbers of base 16 in lowercase.
- *
  * Return: Always 0.
  */
 int main(void)
