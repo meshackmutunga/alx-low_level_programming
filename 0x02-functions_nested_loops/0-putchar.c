d41018fced576adcfa2df1a5a94c7d61c1811b07
bob-aila <61519243+bob-aila@users.noreply.github.com>
2023-02-21 22:38:09 +0300

    Update 0-putchar.c
    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 
 /**
  * 0-putchar - writes the character c to stdout
- * @c: The character to print
+ * @c: The character to print herre
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set
