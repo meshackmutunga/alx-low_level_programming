91cdd4d263e64155dde9e42e742f26f59648fadb
bob-aila <61519243+bob-aila@users.noreply.github.com>
2023-02-23 22:23:13 +0300

    First commit

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of the three numbers
+ * @a: first int
+ * @b: second int
+ * @c: third integerst number
+ */
+
+int largest_number(int a, int b, int c)
+{int largest;
+
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+
+return (largest);
+}
